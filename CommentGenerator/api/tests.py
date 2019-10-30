b8201944a5afafc14d334bd24eae570f24431b61
leonardo <l.frioli@studenti.unipi.it>
2019-10-30 12:08:46 +0100

    added tests for the simple api

--- CommentGenerator/api/tests.py
+++ CommentGenerator/api/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from . import views
+
 
 # Create your tests here.
+class APITestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    # this tests are simple, they will need to be changed
+    def test_api_get(self):
+        response = self.client.get('/api/')
+        content = response.content.decode("utf-8")
+        self.assertEqual(content,'Hello, nice GET')
+
+    def test_api_post(self):
+        response = self.client.post('/api/')
+        content = response.content.decode("utf-8")
+        self.assertEqual(content,'Hello, nice POST')
