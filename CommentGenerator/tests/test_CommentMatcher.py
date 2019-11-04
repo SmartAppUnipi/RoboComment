bc490ab604dd84dd09bbcbcbb8ebeb534998891d
leonardo <l.frioli@studenti.unipi.it>
2019-11-04 15:45:10 +0100

    using unittest TestCase for testing

--- CommentGenerator/tests/test_CommentMatcher.py
+++ CommentGenerator/tests/test_CommentMatcher.py
@@ -1,10 +1,13 @@
 import unittest
 from src.CommentMatcher import CommentMatcher
 
-def test_pick_comment1():
-    commentmatcher = CommentMatcher()
 
-    jsonobj = {}
-    comment = commentmatcher.pick_comment(jsonobj)
+class TestCommentMatcher(unittest.TestCase):
+    def setUp(self):
+        self.comment_matcher = CommentMatcher()
 
-    print(comment)
+    def test_pick_comment1(self):
+        jsonobj = {}
+        comment = self.comment_matcher.pick_comment(jsonobj)
+
+        print(comment)
--- CommentGenerator/tests/test_CommentUpdater.py
+++ CommentGenerator/tests/test_CommentUpdater.py
@@ -1,9 +1,14 @@
 import unittest
 from src.CommentUpdater import CommentUpdater
 
-def test_update_comment1():
-    cu = CommentUpdater()
 
-    updated_comment = cu.update_comment("Cristiano Ronaldo has made a goal")
-    print(updated_comment)
+class TestCommentUpdater(unittest.TestCase):
+
+    def setUp(self):
+        self.comment_updater = CommentUpdater()
+
+    def test_update_comment1(self):
+
+        updated_comment = self.comment_updater.update_comment("Cristiano Ronaldo has made a goal")
+        print(updated_comment)
 
