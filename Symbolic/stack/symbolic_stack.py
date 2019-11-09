5af282f72f4042f87655223c34b372391d868448
Michele Fontana <mikelefonty@gmail.com>
2019-11-09 11:16:51 +0100

    A first very simple implementation of the stack

---
+++ Symbolic/stack/symbolic_stack.py
@@ -0,0 +1,87 @@
+from collections import deque
+
+
+class EmptyQueueError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+class InvalidIndexError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+class NotFoundElementError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+class Stack:
+
+    def __init__(self):
+        self._stack = deque()
+
+    def push(self,v,idx):
+        self._stack.insert(idx,v)
+    
+    def push_front(self,v):
+        self._stack.insert(0,v)
+
+    def pop(self,idx):
+        if self.stack_len() == 0:
+            raise EmptyQueueError('The queue is empty')
+        
+        if self.stack_len() <= idx:
+            raise InvalidIndexError("The element at index {} does not exist".format(idx))
+        
+        element_removed = self._stack[idx] 
+        del self._stack[idx]
+        return element_removed
+    
+    def pop_front(self):
+        return self.pop(0)
+
+    def get_stack(self):
+        return list(self._stack)
+    
+    def get(self,idx):
+        return list(self._stack)[idx]
+    
+    def stack_len(self):
+        return len(self.get_stack())  
+    
+    def duplicate(self):
+        new_stack = Stack()
+        for i in reversed(range(len(list(self._stack)))):
+            new_stack.push_front(self.get(i))
+        return new_stack
+
+    def clear(self):
+       for i in range(self.stack_len()):
+           self.pop_front()
+
+    def find(self,v):
+        found = False
+        idx = 0
+        N = self.stack_len()
+
+        while idx < N and not found:
+            elem = self.get(idx)
+            if elem == v:
+                found = True
+            else:
+                idx += 1
+        
+        if found:
+            return idx
+        else:
+            raise NotFoundElementError("The element is not in the stack")
+
+    def match(self,patterns):
+        print("Match some patterns....")
