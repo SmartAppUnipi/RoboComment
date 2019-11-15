68804f5bf56fa4579965ce703c86de406193ff3e
danilonumeroso <danilo.numeroso@gmail.com>
2019-11-15 16:58:36 +0100

    We bloody did it!

--- kb/src/db/ontologies.js
+++ kb/src/db/ontologies.js
@@ -1,2 +1,27 @@
-const rdf = require('rdflib')
+const rdf = require('rdfstore')
+const fs  = require('fs')
 
+new rdf.Store(async (err, store) => {
+    const rdf = fs.readFileSync('syntax.ttl').toString()
+    store.load('text/turtle', rdf, (s,d) => {
+        store.registerDefaultProfileNamespaces()
+        store.setPrefix("rb", "http://www.semanticweb.org/dvara/ontologies/RoboComment#")
+        store.execute(`PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX : <http://www.semanticweb.org/dvara/ontologies/RoboComment#>
+        SELECT ?name
+        WHERE { 
+         ?Persona :hasName ?name.
+        }`, (success, results) => {console.log(success, results)})
+    })
+})
+
+
+// const rdf = require('rdflib')
+
+// const store = rdf.graph()
+// const me = store.sym("file:///home/danilo/Documents/SA/RoboComment/kb/src/db/RoboComment.owl")
+// const profile = me.doc()
+// const RB = new rdf.Namespace('http://www.semanticweb.org/dvara/ontologies/RoboComment#');
