6834d713c9fd625e6143b13e2d63a1bad081d009
leonardo <l.frioli@studenti.unipi.it>
2019-11-19 16:05:19 +0100

    now the communication with audio is async which is better

--- CommentGenerator/tests/test_api.py
+++ CommentGenerator/tests/test_api.py
@@ -18,24 +18,10 @@ class TestApi(unittest.TestCase):
     
     def test_api_action1(self):
         ''' testing a basic flow of our application'''
-        audio_module_input = {}
-
-        def audio_module(request,context):
-            # this function will mock the audio module 
-            # it stores the json we send to the audio
-            nonlocal audio_module_input
-            audio_module_input = json.loads(request.body)
 
         with open('tests/mock_assets/input1.json', 'r') as json_file:
             input_json = json.load(json_file)
 
-        with requests_mock.mock() as mock_request:
-            # notifying the mock manager to care about a particular post request
-            # x.x.x.x is the default value used to init AUDIO IP 
-            mock_request.post("http://x.x.x.x:3003/", text=audio_module, status_code=200)
-            res = self.client.post("/api/action", data=json.dumps(input_json))
+        res = self.client.post("/api/action", data=json.dumps(input_json))
 
-        # checking if the json we send to audio is well formed
-
-        assert set(audio_module_input.keys()) == set(['comment','emphasis','startTime','endTime','priority'])
-        assert audio_module_input['priority'] >= 0 and audio_module_input['priority'] <= 5
+        assert res.status_code == 200
