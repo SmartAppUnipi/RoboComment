d968f11df88308098a31e6613eb241d5be754683
danilonumeroso <danilo.numeroso@gmail.com>
2019-11-19 17:44:00 +0100

    wip

--- kb/src/db/Ontologies.ts
+++ kb/src/db/Ontologies.ts
@@ -27,11 +27,13 @@ export class Ontologies {
 export class Query {
 
     private static readonly header: string = `
-    PREFIX : <http://www.semanticweb.org/dvara/ontologies/RoboComment#>
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX owl: <http://www.w3.org/2002/07/owl#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>`
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX info: <http://somewhere/peopleInfo#>
+    PREFIX vcard: <http://www.w3.org/2001/vcard-rdf/3.0#>
+    PREFIX : <http://www.semanticweb.org/dvara/ontologies/RoboComment#>`
 
     public static readonly get_club = (id: number) => `
     ${Query.header}
@@ -47,21 +49,40 @@ export class Query {
 
     public static readonly get_persona = (id: number) => `
     ${Query.header}
-    SELECT ?hasPlayerName ?wasBornIn ?teamName ?height ?wasBornOn
+    SELECT ?name ?wasBornIn ?club ?height ?wasBornOn
     WHERE {
         ?wyid :wyid "${id}".
-        ?wyid :hasName ?hasPlayerName.
-        ?wyid :wasBornIn ?wasBornIn.
-        ?wyid :height ?height.
-        ?wyid :wasBornOn ?wasBornOn.
+        ?wyid :hasName ?name.
 
-        ?PlayerCareerStation :isPersona :${id}.
-        ?PlayerCareerStation :isMember ?isMember.
+        OPTIONAL {
+            ?wyid :height ?heightS
+        } .
 
-        ?isMember :teamOf ?teamOf.
-        ?teamOf :hasName ?teamName.
+        OPTIONAL {
+            ?wyid :wasBornIn ?wasBornIn
+        } .
+
+        OPTIONAL {
+            ?wyid :wasBornOn ?wasBornOn
+        }
     }
     `
+
+    // public static readonly get_persona = (id: number) => `
+    // ${Query.header}
+    // SELECT ?name ?wasBornIn ?club ?wasBornOn
+    // WHERE {
+    //     ?wyid :wyid "${id}".
+    //     ?wyid :hasName ?name.
+    //     ?wyid :wasBornIn ?wasBornIn.
+    //     ?wyid :wasBornOn ?wasBornOn.
+
+    //     ?CoachCareerStation :isPersona :${id} .
+    //     ?CoachCareerStation :isMember ?isMember.
+
+    //     ?isMember :teamOf ?teamOf.
+    // }
+    // `
 
     public static readonly get_match = (id: number) => `
     ${Query.header}
