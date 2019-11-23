278b4ca8f878405ce2815eafcacb741e96b84018
lorenzospano <lnz.spano@gmail.com>
2019-11-23 13:39:58 +0100

    Fixed set_rule_matcher

--- Symbolic/server/game_model/set_rule_matcher.py
+++ Symbolic/server/game_model/set_rule_matcher.py
@@ -3,6 +3,4 @@ from rule_matcher import rule_matcher
 
 def set_rule_matcher(rules):
     for rule in rules:
-        if not rule_matcher(rule.condition, rule.action):
-            return False
-    return True
+        rule_matcher(rule.condition, rule.action)
