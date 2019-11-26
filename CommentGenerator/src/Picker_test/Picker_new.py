d69d96cf38d32f99e49f56a64491900de29ad4b0
Lucastraz <strazzeraluca@gmail.com>
2019-11-26 16:59:14 +0100

    Update new_picker to works when input json is empty

--- CommentGenerator/src/Picker_test/Picker_new.py
+++ CommentGenerator/src/Picker_test/Picker_new.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 from CommentGenerator.src.Picker_test.Tagger import Tagger
 from CommentGenerator.src.Picker_test.TemplateGenerator import TemplateGenerator
@@ -8,6 +9,10 @@ class Picker:
     def __init__(self):
         self.tagger = Tagger()
         self.template_generator = TemplateGenerator()
+        others = './comments_others.txt'
+        # list of possible comments to extract where no info are passed
+        with open(others, "r") as f:
+            self.comment_others = [str(line) for line in f.readlines()]
 
         # order of tags
         self.sentence_order = ['player1', 'team1', 'subtype', 'field_zone', 'player2', 'team2']
@@ -17,20 +22,25 @@ class Picker:
     def pick_comment(self, input_json: json):
         ''' From json extract information and save in a ordered list according to sentence_order '''
 
-        information = input_json['details']
-
+        print("Input:", input_json)
+        comment = ""
         # extract information from json
-        sentence = self.create_sentence(information)
-        print("\nResulting sentence:", sentence)
+        # if json is empty
+        if self.check_empty_json(input_json):
+            return random.sample(self.comment_others, 1)[0]
+        # if json is not empty
+        else:
+            sentence = self.create_sentence(input_json['details'])
+            print("\nTransformed sentence:", sentence)
 
-        # tag the information in the json
-        sentence_tagged = self.tagger.tag_sentence(sentence)
-        print("\nResulting sentence tagged:", sentence_tagged)
+            # tag the information in the json
+            sentence_tagged = self.tagger.tag_sentence(sentence)
+            print("\nTransformed sentence tagged:", sentence_tagged)
 
-        #
-        comment = self.template_generator.generate(sentence_tagged)
-        print("\nComment produced:", comment)
-        return
+            # create base sentence
+            comment = self.template_generator.generate(sentence_tagged)
+
+        return comment
 
     def create_sentence(self, information):
         """
@@ -56,13 +66,29 @@ class Picker:
 
         return sentence
 
+    def check_empty_json(self, input_json)->bool:
+        """
+        Check if the json has useful information, if not create a contextual template
+        :param input_json:
+        :return:
+        """
+        if len(input_json) == 0:
+            return True
+        else:
+            return False
+
 
 if __name__ == '__main__':
 
+    """
+    Call this class from commentator
+    """
     picker = Picker()
 
     with open("../../assets/input1.json", 'r') as input1_json:
         input_json = json.load(input1_json)
 
+        input_json = {}
         comment = picker.pick_comment(input_json)
+        print("\nFINAL comment:", comment)
 
