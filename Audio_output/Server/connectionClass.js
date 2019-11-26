673eef238d8be59b831b4ebe5616f46f58a0f2e0
Gabriele <gabriele.barreca93@hotmail.com>
2019-11-27 00:42:27 +0100

    add new class element

--- Audio_output/Server/connectionClass.js
+++ Audio_output/Server/connectionClass.js
@@ -1,6 +1,8 @@
 class ConnectionUser {
     constructor(socket, id) {
-        this.socket          = socket;
-        this.id     = id;
+        this.socket      = socket;
+        this.id          = id;
+        this.new_comment = null;
+        this.old_comment = null;
     }
 }
