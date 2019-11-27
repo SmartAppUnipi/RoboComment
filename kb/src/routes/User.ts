7f8d835f5a071562f241a776dbdc7169e2b58f24
leonardo <l.frioli@studenti.unipi.it>
2019-11-27 17:16:17 +0100

    added kb manually

---
+++ kb/src/routes/User.ts
@@ -0,0 +1,77 @@
+import { Request, Response, Router, Express } from 'express'
+import { BAD_REQUEST, OK } from 'http-status-codes'
+import { UserDao } from '../daos'
+import { ParamsDictionary } from 'express-serve-static-core'
+
+// Init shared
+const router = Router()
+
+router.post('/', (req, res) => {
+    try {
+        let { user } = req.body
+        if (!user) {
+            return res.status(BAD_REQUEST).json({
+                error: 'user is missing in body',
+            })
+        }
+        console.info(user)
+        user = UserDao.add(user)
+        return res.status(OK).json(user)
+    } catch (err) {
+        console.error(err.message, err)
+        return res.status(BAD_REQUEST).json({
+            error: err.message,
+        })
+    }
+})
+
+router.post('/login', (req, res) => {
+    try {
+        let { user } = req.body
+        if (!user) {
+            return res.status(BAD_REQUEST).json({
+                error: 'user is missing in body',
+            })
+        }
+        user = UserDao.login(user.email, user.password)
+        return res.status(OK).json(user)
+    } catch (err) {
+        console.error(err.message, err)
+        return res.status(BAD_REQUEST).json({
+            error: err.message,
+        })
+    }
+})
+
+router.put('/', (req, res) => {
+    try {
+        let { user } = req.body
+        if (!user) {
+            return res.status(BAD_REQUEST).json({
+                error: 'user is missing in body',
+            })
+        }
+        user = UserDao.update(user)
+        return res.status(OK).json(user)
+    } catch (err) {
+        // logger.error(err.message, err)
+        return res.status(BAD_REQUEST).json({
+            error: err.message,
+        })
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    try {
+        const { id } = req.params as ParamsDictionary
+        const user = UserDao.delete(Number(id))
+        return res.status(OK).end()
+    } catch (err) {
+        // logger.error(err.message, err);
+        return res.status(BAD_REQUEST).json({
+            error: err.message,
+        })
+    }
+})
+
+export default router
