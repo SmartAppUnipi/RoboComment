83951614970ec1e79711eacd6cd492fb9211d1c8
danilonumeroso <danilo.numeroso@gmail.com>
2019-11-27 20:21:24 +0100

    update

--- kb/src/db/Ontologies.ts
+++ kb/src/db/Ontologies.ts
@@ -133,23 +133,17 @@ export class Query {
     `
 }
 
-    public static readonly get_match = (id1: number, id2:number, id3:number) => `
-    ${Query.header}
-    SELECT  ?Match
-	WHERE
-  { ?Match  :homeTeam  ?x .
-    ?x      :teamOf    ?x1 .
-    ?x1     :hasName   "${id1}" .
-    ?Match  :awayTeam  ?y .
-    ?y      :teamOf    ?y1 .
-    ?y1     :hasName   "${id2}" .
-    ?Match  :date      "${id3}"
-  }
-    `
-}
-
-
-
-
-
-
+//     public static readonly get_match = (id1: number, id2:number, id3:number) => `
+//     ${Query.header}
+//     SELECT  ?Match
+// 	WHERE
+//   { ?Match  :homeTeam  ?x .
+//     ?x      :teamOf    ?x1 .
+//     ?x1     :hasName   "${id1}" .
+//     ?Match  :awayTeam  ?y .
+//     ?y      :teamOf    ?y1 .
+//     ?y1     :hasName   "${id2}" .
+//     ?Match  :date      "${id3}"
+//   }
+    // `
+// }
