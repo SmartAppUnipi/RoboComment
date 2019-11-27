9df9cf60b6220e7d877c2dc9791e2eca1c49f420
Lucastraz <strazzeraluca@gmail.com>
2019-11-27 09:36:48 +0100

    Update leaf of Template to generate diversified comments

--- CommentGenerator/src/Picker_test/TemplateGenerator.py
+++ CommentGenerator/src/Picker_test/TemplateGenerator.py
@@ -2,52 +2,55 @@ import random
 
 
 class TemplateGenerator:
-
+    """
+    In the leaf variable are stored the leaf possible value corresponding to the leaf defined in the grammar.
+    The file json_grammar will solve you doubt about the node meaning
+    """
     def __init__(self):
         self.leaf = {
             "Active_player": {
-                "empty" : ["a player"],
-                "no_empty": ["{p}"]
+                "empty": ["a player", "so the player", " a player, therefore,"],
+                "no_empty": ["{p}, {p}, therefore"]
             },
             "Passive_player": {
-                "empty": ["a player"],
-                "no_empty": ["{p}"]
+                "empty": ["a player", "so the player", "therefore, the player"],
+                "no_empty": ["{p}", "therefore {p}"]
             },
             "Team_player": {
                 "no_empty": ["of the {p}", "belonging to {p}", ", a {p} player,", "of the {p} team", ""]
             },
             "Team_receiver": {
-                "no_empty": ["of the {p}", "belonging to {p}", ", a {p} player,", "of the {p} team", ""]
+                "no_empty": ["belonging to {p}", "of the {p}", "of the {p} team", "", ", a {p} player,"]
             },
             "Action_active": {
-                "no_empty": ["do a {p}", "{p}", "make a {p}"]
+                "no_empty": ["do a {p}", "perform a {p}", "{p}", "make a {p}"]
             },
             "Action_active_continue": {
-                "no_empty": ["is doing {p}", "continuously {p}"]
+                "no_empty": ["is doing {p}", "continuously {p}", "keep doing {p}"]
             },
             "Action_active_ball": {
-                "no_empty": ["{p} the ball", "{p}", "make a {p}"]
+                "no_empty": ["{p} the ball", "execute a {p}", "{p}", "make a {p}"]
             },
             "Action_passive": {
-                "no_empty": ["receives a {p}", "has a {p}"]
+                "no_empty": ["receives a {p}", "has a {p}", "obtain a {p}"]
             },
             "Action_passive_continue": {
                 "no_empty": ["is receiving {p}", "continuously {p}"]
             },
             "Action_passive_ball": {
-                "no_empty": ["receive the ball", "get a {p}"]
+                "no_empty": ["receive the ball", "get a {p}", "obtain the ball"]
             },
             "Action_zone_from": {
-                "no_empty": ["from {p} of the field", "from the {p}"]
+                "no_empty": ["from {p} of the field", "from the {p}", "arrive from {p}"]
             },
             "Action_zone_in": {
-                "no_empty": ["in the {p} of the field", "inside {p} of the field"]
+                "no_empty": ["in the {p} of the field", "inside {p} of the field", "cutting {p} part of the field", "inner {p}"]
             },
             "Receiver_player": {
-                "no_empty": ["to {p}", "towards the {p}"]
+                "no_empty": ["to {p}", "towards the {p}", "direct to {p}"]
             },
-            "Does_player":{
-                "no_empty": ["from {p}", "by the player", "by {p}"]
+            "Does_player": {
+                "no_empty": ["from {p}", "by the player", "by {p}", "coming out of {p}"]
             }
         }
 
