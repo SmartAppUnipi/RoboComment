cc6bc316a258d1978ea6af479aff7dea598e8197
lorenzospano <lnz.spano@gmail.com>
2019-11-29 17:02:05 +0100

    Implemented fire and check

--- Symbolic/server/game_model/interpreter/rule_matcher.py
+++ Symbolic/server/game_model/interpreter/rule_matcher.py
@@ -1,4 +1,6 @@
 from game_model.interpreter.boolean_matcher import boolean_matcher
+from game_model.interpreter.rts import check, fire
+from game_model.game_model import GameModel
 import types
 from collections import deque    #Used by action
 
@@ -15,14 +17,13 @@ if all the conditions match in AND then the action is executed
 '''
 
 
-def rule_matcher(condition, action, constraints, stacks, registers):
-    '''
-    if not isinstance(action, types.FunctionType) or not isinstance(constraints, type.FunctionType):
+def rule_matcher(condition, action, constraints):
+    if not isinstance(action, str) or not isinstance(constraints, str):
         raise TypeError
-    '''
+    stacks, registers = GameModel.get_env()
     if boolean_matcher(condition, stacks, registers):
-        if constraints(registers):
-            action(stacks, registers)
+        if check(constraints):
+            fire(action)
             return True
     return False
     # Probably return value is not needed since action is performed here
--- Symbolic/server/game_model/interpreter/set_rule_matcher.py
+++ Symbolic/server/game_model/interpreter/set_rule_matcher.py
@@ -3,4 +3,4 @@ from game_model.interpreter.rule_matcher import rule_matcher
 
 def set_rule_matcher(rules, stacks):
     for rule in rules:
-        rule_matcher(rule.condition, rule.action, stacks)
+        rule_matcher(rule.condition, rule.action, rule.constraints)
