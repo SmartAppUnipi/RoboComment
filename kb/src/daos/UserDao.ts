2b960f1bbfd6ba317e5c7810371d24bfccf9720f
danilonumeroso <danilo.numeroso@gmail.com>
2019-12-01 21:56:28 +0100

    Enhancements

--- kb/src/daos/UserDao.ts
+++ kb/src/daos/UserDao.ts
@@ -1,61 +1,59 @@
 import * as Entities from '../entities'
 import * as DB from '../db'
 import * as Errors from '../errors'
-import e = require('express')
 
 export class UserDao {
 
-    public static login(email: string, pwd: string): Entities.User {
-        const db = DB.Users.data
+  public static login(email: string, pwd: string): Entities.User {
+    const db = DB.Users.data
 
-        const user = db.users.find(user => user.email === email && user.password === pwd)
+    const user = db.users.find(user => user.email === email && user.password === pwd)
 
-        if (user === undefined) {
-            throw Error(Errors.USER_NOT_FOUND)
-        }
-
-        delete user.password
-        return user
+    if (user === undefined) {
+      throw Error(Errors.USER_NOT_FOUND)
     }
 
-    public static add(user: Entities.User): Entities.User {
-        const db = DB.Users.data
+    return { ...user }
+  }
 
-        if (db.users.some(value => value.email === user.email)) {
-            throw Error(Errors.EMAIL_ALREADY_USED)
-        }
+  public static add(user: Entities.User): Entities.User {
+    const db = DB.Users.data
 
-        user.id = db.users.length + 1
-        db.users.push(user)
-
-        DB.Users.save_db()
-        return user
+    if (db.users.some(value => value.email === user.email)) {
+      throw Error(Errors.EMAIL_ALREADY_USED)
     }
 
-    public static update(user: Entities.User): void {
-        const db = DB.Users.data
+    user.id = db.users.length + 1
+    db.users.push(user)
 
-        const i = db.users.findIndex(old_user => old_user.id === user.id)
-        const old_user = db.users[i]
+    DB.Users.save_db()
+    return { ...user }
+  }
 
-        if (old_user === undefined) {
-            throw Error(Errors.USER_NOT_FOUND)
-        }
+  public static update(user: Entities.User): void {
+    const db = DB.Users.data
 
-        if (old_user === undefined) {
-            throw Error(Errors.USER_NOT_FOUND)
-        }
+    const i = db.users.findIndex(old_user => old_user.id === user.id)
+    const old_user = db.users[i]
 
-        db.users[i] = { ...user, password: old_user.password } as Entities.User
-
-        DB.Users.save_db()
+    if (old_user === undefined) {
+      throw Error(Errors.USER_NOT_FOUND)
     }
 
-    public static delete(id: number): void {
-        const db = DB.Users.data
-
-        db.users = db.users.filter(user => user.id !== id)
-
-        DB.Users.save_db()
+    if (old_user === undefined) {
+      throw Error(Errors.USER_NOT_FOUND)
     }
+
+    db.users[i] = { ...user, password: old_user.password } as Entities.User
+
+    DB.Users.save_db()
+  }
+
+  public static delete(email: string): void {
+    const db = DB.Users.data
+
+    db.users = db.users.filter(user => user.email !== email)
+    console.log(db.users)
+    DB.Users.save_db()
+  }
 }
--- kb/test/test.ts
+++ kb/test/test.ts
@@ -4,14 +4,15 @@ import jsonfile from 'jsonfile'
 import { DB_USERS } from '../config'
 
 const EmptyDB = {
-    users: []
+  users: []
 }
 
+fs.writeFileSync(`${DB_USERS}.backup`, fs.readFileSync(DB_USERS))
 jsonfile.writeFileSync(DB_USERS, EmptyDB, {
-    spaces: 4
+  spaces: 4
 })
 
 newman.run({
-    collection: require('./tests.json'),
-    reporters: 'cli'
+  collection: require('./tests.json'),
+  reporters: 'cli'
 })
