6166dcd0171a12ac9b11250afdea2b7a4fd6a92b
Lucastraz <strazzeraluca@gmail.com>
2019-12-02 12:30:13 +0100

    Add mock json for the new input from symbolic

--- CommentGenerator/tests/test_Adapter.py
+++ CommentGenerator/tests/test_Adapter.py
@@ -8,7 +8,7 @@ class TestFiller(unittest.TestCase):
 
     def test_adapt_comment1(self):
         input1 = None
-        with open("CommentGenerator/tests/mock_assets/elementary/pass/input_real1.json", 'r') as mock_json:
+        with open("CommentGenerator/tests/mock_assets/elementary/pass/input_symbolic1.json", 'r') as mock_json:
             input1 = json.load(mock_json)
         jsonobj = self.adapter.adapt(input1)
 
