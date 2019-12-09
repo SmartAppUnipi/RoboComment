ee56f18af6beb7a87ad04ef2536df202a7ca1f97
Lucastraz <strazzeraluca@gmail.com>
2019-12-09 10:25:04 +0100

    Filler now works properly substituting the placeholders (team, player)

--- CommentGenerator/src/Filler.py
+++ CommentGenerator/src/Filler.py
@@ -20,10 +20,29 @@ template_modifiers = {
 class Filler:
 
     def __init__(self, kb):
-        self.kb = kb
+        self.__kb = kb
 
 
-    def update_comment(self, comment:str, placeholders:dict):
+    def update_comment(self, comment:str, placeholders:dict)->str:
+        """
+        Update commet with placeholders value, querying the kb
+        :param comment:
+        :param placeholders:
+        :return:
+        """
+
+        for placeh in placeholders:
+            new_value = "ERROR"
+            if placeh == 'player1' or placeh == 'player2':
+                pass
+                #new_value = self.__kb.get_player(placeholders[placeh])
+            elif placeh == 'team1' or placeh == 'team2':
+                pass
+                #new_value = self.__kb.get_team(placeholders[placeh])
+
+            comment = comment.replace("{"+placeh+"}", str(new_value))
+
+        """
 
         # getting the placeholders {*_modifier} 
         placeholders = re.findall(r'{(.*?)}', comment)
@@ -47,11 +66,14 @@ class Filler:
                 details[mod] = template_modifiers[bias][mod][0]
 
         comment = comment.format(**details)
+        
+        """
 
         return comment
-    
+
+    """
     def replace_id_with_names(self,details):
-        def check_and_replace(key,details, get_from_kb):
+        def check_and_replace(key:str, details, get_from_kb):
             if key in details.keys():
                 if  details[key] != '{empty}':
                     details[key] = get_from_kb(details[key])
@@ -65,12 +87,16 @@ class Filler:
         details = check_and_replace('player2', details, self.kb.get_player)
         
         return details
+    """
 
 if __name__ == '__main__':
 
     comment = "it seems that {player1} belonging to {team1} intercepts the ball of the player"
     placeholders = {'player1': 42, 'team1': 42}
 
+    print("COMMENT:", comment)
+    print("PLACEHOLDERS:", placeholders)
+
     filler = Filler("")
-    comment = filler.update_comment(test1, 0)
+    comment = filler.update_comment(comment, placeholders)
     print(comment)
