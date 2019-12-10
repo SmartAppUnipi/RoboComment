1129f4cb5333ce51db54b18425f44dc9c9007034
danilonumeroso <danilo.numeroso@gmail.com>
2019-12-10 17:06:25 +0100

    add penalty

--- Symbolic/server/game_model/interpreter/opcodes.py
+++ Symbolic/server/game_model/interpreter/opcodes.py
@@ -84,7 +84,7 @@ def push_closest(stack_name, pos):
             y = float(player['position']['y'])
             distance = math.sqrt(((ball_x - x)**2)+((ball_y - y)**2))
             player['delta'] = distance
-    
+
     min_delta = min([a['delta'] for a in pos['players']])
 
     closest = None
@@ -115,22 +115,22 @@ def ball_on_target(pos):
     _goal_height = 7
 
     _threshold = 1
-    
+
     ball_pos_x = pos['ball'][0]['position']['x']
     ball_pos_y = pos['ball'][0]['position']['y']
 
     if (_field_height / 2 - _goal_height / 2) <= ball_pos_y <=  (_field_height / 2 + _goal_height / 2):
         if  0 <= ball_pos_x <= _threshold or _field_width - _threshold <= ball_pos_x <= _field_width:
             return True
-    
-    return False 
+
+    return False
 
 def ball_off_target(pos):
     _field_width = 105
     _field_height = 68
 
     _goal_height = 7
-    
+
     ball_pos_x = pos['ball'][0]['position']['x']
     ball_pos_y = pos['ball'][0]['position']['y']
 
@@ -139,16 +139,14 @@ def ball_off_target(pos):
     elif (ball_pos_y > (_field_height / 2) + (_goal_height / 2) or ball_pos_y < (_field_height / 2) - (_goal_height / 2)) and (ball_pos_x >= _field_width):
         return True
 
-    return False  
-
-
+    return False
 
 def ball_goal(pos):
     _field_width = 105
     _field_height = 68
 
     _goal_height = 7
-    
+
     ball_pos_x = pos['ball'][0]['position']['x']
     ball_pos_y = pos['ball'][0]['position']['y']
 
@@ -156,8 +154,43 @@ def ball_goal(pos):
         if  ball_pos_x <= 0 or ball_pos_x >= _field_width:
             return True
 
-    return False 
-    
+    return False
+
+def is_penalty(pos):
+    _field_width = 68
+    _field_height = 105
+
+    _goal_area_height = 16
+    _goal_area_width  = 40
+
+    no_players_in_goal_area = 0
+    no_players_out_of_goal_area = 0
+
+    for p in pos['players']:
+        if p["team"] == -1: # p is referee
+            continue
+
+        x = p["position"]["x"]
+        y = p["position"]["y"]
+
+        if 0 <= x <= _goal_area_height and \
+           (_field_width / 2 + goal_area_width / 2 <= y <= \
+            _field_width / 2 - goal_area_width / 2) \
+           or _field_height - _goal_area_height >= x and \
+           (_field_width / 2 + goal_area_width / 2 <= y <= \
+            _field_width / 2 - goal_area_width / 2):
+
+            no_players_in_goal_area += 1
+            continue
+
+        if _goal_area_height - 2 >= x >= _goal_area_height + 2 or \
+           (_goal_area_height - 2) <= _field_height - x <= _goal_area_height + 2:
+            no_players_out_of_goal_area += 1
+
+    if no_players_in_goal_area == 2 and no_players_out_of_goal_area >= 4:
+        return True
+
+    return False
 
 def trunc(a):
     return int(a * 10000) / 10000
