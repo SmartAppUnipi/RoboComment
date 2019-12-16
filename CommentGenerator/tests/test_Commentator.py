099bb68c9da018ac59a9ac2661dcb1a195ce2f2d
leonardo <l.frioli@studenti.unipi.it>
2019-12-16 15:40:34 +0100

    added another test

--- CommentGenerator/tests/test_Commentator.py
+++ CommentGenerator/tests/test_Commentator.py
@@ -23,6 +23,8 @@ class TestCommentator(unittest.TestCase):
         self.shot_on_target = self.get_symbolic_intput('shot_on_target/input_symbolic1.json')
         self.tikitaka = self.get_symbolic_intput('tikitaka/input_symbolic1.json')
 
+        self.skip_welcome_message()
+
     def get_symbolic_intput(self, file_path):
         assets = "CommentGenerator/tests/mock_assets/elementary/"
         input_json = ""
@@ -40,79 +42,63 @@ class TestCommentator(unittest.TestCase):
         assert set(output.keys()) == set(['comment','emphasis','startTime','endTime','priority','id','language','voice'])
         assert 0 <= output['priority'] and output['priority'] <= 5
 
-    def test_run2(self):
-        self.skip_welcome_message()
-        
+    def test_just_intercept(self):
         output = self.commentator.run(self.intercept_input)
-
         print(output['comment'])
     
-    def test_run3(self): 
-        self.skip_welcome_message()
-        
+    def test_just_possession(self):         
         output = self.commentator.run(self.possession_input)
-
         print(output['comment'])
     
-    def test_run4(self): 
-        self.skip_welcome_message()
-        
+    def test_just_pass(self):         
         output = self.commentator.run(self.pass_input)
-
         print(output['comment'])
 
-    def test_run5(self):
-        self.skip_welcome_message()
-
+    def test_just_duel(self):
         output = self.commentator.run(self.duel_input)
-
         print(output['comment'])
 
-    def test_run6(self):
-        self.skip_welcome_message()
-
+    def test_just_shotoff(self):
         output = self.commentator.run(self.shot_off_target)
-
         print(output['comment'])
 
-    def test_run7(self):
-        self.skip_welcome_message()
-
+    def test_just_shot_on(self):
         output = self.commentator.run(self.shot_on_target)
-
         print(output['comment'])
 
-    def test_run8(self):
-        self.skip_welcome_message()
-
+    def test_just_tikitaka(self):
         output = self.commentator.run(self.tikitaka)
-
         print(output['comment'])
 
-    def test_run9(self):
-        self.skip_welcome_message()
-
+    def test_just_revoked_goal(self):
         output = self.commentator.run(self.revoked_goal_input)
-
         print(output['comment'])
 
-    def test_run10(self):
-        self.skip_welcome_message()
-
+    def test_just_goal(self):
         output = self.commentator.run(self.goal_input)
-
         print(output['comment'])
 
-    def test_run11(self):
-        self.skip_welcome_message()
-
+    def test_just_offside(self):
         output = self.commentator.run(self.offside_input)
 
         print(output['comment'])
 
-    def test_run12(self):
-        self.skip_welcome_message()
-
+    def test_just_penality(self):
         output = self.commentator.run(self.penalty_input)
+        print(output['comment'])
 
+    
+    def test_run1(self):
+        print("RUN: pass,pass,pass, intecept,possession,goal")
+        output = self.commentator.run(self.pass_input)
+        print(output['comment'])
+        output = self.commentator.run(self.pass_input)
+        print(output['comment'])
+        output = self.commentator.run(self.pass_input)
+        print(output['comment'])
+        output = self.commentator.run(self.intercept_input)
+        print(output['comment'])
+        output = self.commentator.run(self.possession_input)
+        print(output['comment'])
+        output = self.commentator.run(self.goal_input)
         print(output['comment'])
