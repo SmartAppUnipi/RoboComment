79a0ff79047377fac9247c64f11398f34f93e149
andrea-rosasco <andrearosasco.ar@gmail.com>
2019-12-16 18:51:54 +0100

    improved rephraser

--- CommentGenerator/src/Rephraser.py
+++ CommentGenerator/src/Rephraser.py
@@ -7,6 +7,7 @@ class Rephraser:
 
     def __init__(self, source):
         self.source = source
+        self.safe_pivots = ['it', 'fr', 'iw', 'tr', 'he', 'el', 'sq', 'ru', 'ar', 'lb']
 
     def rephrase(self, comment, pivot):
         translator = Translator()
@@ -15,12 +16,14 @@ class Rephraser:
         comment = translator.translate(comment, src=pivot[len(pivot) - 1], dest=self.source).text
         return comment
 
-    def random_rephrase(self, comment, k=2):
-        pivot = []
+    def random_rephrase(self, comment, r=1, s=3, languages=list(googletrans.LANGUAGES.keys())):
+        for i in range(s):
+            pivot = []
+            for j in range(r):
+                idx = randrange(len(languages))
+                pivot.append(languages.pop(idx))
+            comment = self.rephrase(comment, pivot)
+        return comment
 
-        languages = list(googletrans.LANGUAGES.keys())
-        for i in range(k):
-            idx = randrange(len(languages))
-            pivot.append(languages.pop(idx))
-
-        return self.rephrase(comment, pivot)
+    def safe_random_reprase(self, comment, r=1, s=3):
+        return self.random_rephrase(comment, r, s, self.safe_pivots)
